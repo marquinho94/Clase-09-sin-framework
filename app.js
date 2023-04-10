8f4d5d6b8e763980a6a1e3b14cf132eea90760ad
marquinho94 <marcoalanmiranda@gmail.com>
2023-04-10 11:29:02 -0300

    Proyecto inicial pelado sin instalar paquetes

---
+++ app.js
@@ -0,0 +1,10 @@
+let myApp = document.getElementById("myApp");
+
+myApp.innerHTML = "hola nene"
+
+myApp,innerHTML= myApp.innerHTML + `
+                                        <div id="reactApp"></div>
+                                    `
+                                    ;
+
+                                    
